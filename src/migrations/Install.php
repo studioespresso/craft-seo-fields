862a58005cf8094610fb35d71b96ce3934eec3eb
Jan Henckens <jan@studioespresso.co>
2019-01-11 22:10:08 +0100

    Basic install migration

---
+++ src/migrations/Install.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace studioespresso\seofields\migrations;
+
+use Craft;
+use craft\db\Migration;
+use studioespresso\seofields\records\DefaultsRecord;
+
+/***
+ * @author    Studio Espresso
+ * @package   SeoFields
+ * @since     1.0.0
+ */
+class Install extends Migration
+{
+    // Public Properties
+    // =========================================================================
+    public $driver;
+
+    // Public Methods
+    // =========================================================================
+    public function safeUp()
+    {
+        $this->driver = Craft::$app->getConfig()->getDb()->driver;
+        if ($this->createTables()) {
+            $this->addForeignKeys();
+            // Refresh the db schema caches
+            Craft::$app->db->schema->refresh();
+        }
+
+        return true;
+    }
+
+    public function safeDown()
+    {
+        $this->driver = Craft::$app->getConfig()->getDb()->driver;
+        $this->removeTables();
+        return true;
+    }
+
+    // Protected Methods
+    // =========================================================================
+    protected function createTables()
+    {
+        $tablesCreated = false;
+        $tableSchema = Craft::$app->db->schema->getTableSchema(DefaultsRecord::tableName());
+        if ($tableSchema === null) {
+            $tablesCreated = true;
+            $this->createTable(
+                DefaultsRecord::tableName(),
+                [
+                    'id' => $this->primaryKey(),
+                    'siteId' => $this->integer(11)->notNull(),
+                    'dateCreated' => $this->dateTime()->notNull(),
+                    'dateUpdated' => $this->dateTime()->notNull(),
+                    'uid' => $this->uid(),
+                ]
+            );
+        }
+        return $tablesCreated;
+    }
+
+    protected function addForeignKeys()
+    {
+        // $name, $table, $columns, $refTable, $refColumns, $delete = null, $update = null)
+        $this->addForeignKey(
+            $this->db->getForeignKeyName(DefaultsRecord::tableName(), 'siteId'),
+            DefaultsRecord::tableName(),
+            'siteId',
+            '{{%sites}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    protected function removeTables()
+    {
+        $this->dropTableIfExists(DefaultsRecord::tableName());
+    }
+}
