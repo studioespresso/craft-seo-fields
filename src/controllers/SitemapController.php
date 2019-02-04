97ca31dd79336c260d12b336244cd1757bff62c3
Jan Henckens <jan@studioespresso.co>
2019-02-04 20:56:40 +0100

    Sitemap controller

---
+++ src/controllers/SitemapController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace studioespresso\seofields\controllers;
+
+use Craft;
+use craft\helpers\Template;
+use craft\web\Controller;
+use studioespresso\seofields\models\SeoDefaultsModel;
+use studioespresso\seofields\records\DefaultsRecord;
+use studioespresso\seofields\SeoFields;
+use yii\helpers\StringHelper;
+
+class SitemapController extends Controller
+{
+    public $allowAnonymous = ['actionRender'];
+
+    public function actionIndex()
+    {
+        $primarySite = Craft::$app->sites->getPrimarySite();
+        $this->redirect("seo-fields/sitemap/$primarySite->handle");
+    }
+
+    public function actionSettings($siteHandle = null)
+    {
+        $currentSite = Craft::$app->sites->getSiteByHandle($siteHandle);
+        Craft::$app->sites->setCurrentSite($currentSite);
+        $data = SeoFields::$plugin->defaultsService->getDataBySite($currentSite);
+        return $this->renderTemplate('seo-fields/_sitemap', [
+            'data' => $data
+        ]);
+    }
+}
