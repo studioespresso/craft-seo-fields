9e7610ba017b3059097ba8df2c613f458a71b4e0
Jan Henckens <jan@studioespresso.co>
2019-03-15 21:05:29 +0100

    Example config

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SEO Fields config.php
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'seo-fields.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    'fieldHandle' => 'seo'
+];
