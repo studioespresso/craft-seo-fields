46d7810cd668f0a3474741ca1719ecb583f3471c
Jan Henckens <jan@studioespresso.co>
2019-03-25 21:51:03 +0100

    Docker upgrade before installing yarn

--- .deploy-docs/entrypoint.sh
+++ .deploy-docs/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -l
-
-sh -c "echo $*"
-env
-cd docs
-pwd
-
-apt-get install curl
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install curl
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get install yarn
