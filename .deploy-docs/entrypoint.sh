8d8892e5a4d890964a62900f4ae193ec92409a28
Jan Henckens <jan@studioespresso.co>
2019-03-25 18:55:10 +0100

    Install yarn

--- .deploy-docs/entrypoint.sh
+++ .deploy-docs/entrypoint.sh
@@ -4,4 +4,8 @@ sh -c "echo $*"
 env
 cd docs
 pwd
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get install yarn
 yarn install
