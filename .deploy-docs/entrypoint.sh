a819668683b7a182089e23826e4cf3456a7a9152
Jan Henckens <jan@studioespresso.co>
2019-03-25 18:58:05 +0100

    Install curl

--- .deploy-docs/entrypoint.sh
+++ .deploy-docs/entrypoint.sh
@@ -5,7 +5,8 @@ env
 cd docs
 pwd
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install yarn
+apt-get install curl
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get install yarn
 yarn install
