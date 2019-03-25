3b50c5d5085cc0a461374635d359d0729f4ba742
Jan Henckens <jan@studioespresso.co>
2019-03-25 22:04:37 +0100

    Install npm first

--- .deploy-docs/entrypoint.sh
+++ .deploy-docs/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install curl
+apt-get -y install curl software-properties-common gnupg apt-transport-https
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get -y install yarn
