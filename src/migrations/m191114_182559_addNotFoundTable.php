fb7602482983694559d634fab173879cf5b60e3e
Jan Henckens <jan@studioespresso.co>
2019-11-14 19:35:36 +0100

    Not found model & migration

---
+++ src/migrations/m191114_182559_addNotFoundTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace studioespresso\seofields\migrations;
+
+use Craft;
+use craft\db\Migration;
+use studioespresso\seofields\records\NotFoundRecord;
+
+/**
+ * m191114_182559_addNotFoundTable migration.
+ */
+class m191114_182559_addNotFoundTable extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $tablesCreated = false;
+        $tableSchema = Craft::$app->db->schema->getTableSchema(NotFoundRecord::tableName());
+        if ($tableSchema === null) {
+            $tablesCreated = true;
+            $this->createTable(
+                NotFoundRecord::tableName(),
+                [
+                    'id' => $this->primaryKey(),
+                    'siteId' => $this->integer(11)->notNull(),
+                    'url' => $this->text(),
+                    'handled' => $this->boolean()->defaultValue(0),
+                    'counter' => $this->bigInteger(),
+                    'dateLastHit' => $this->dateTime()->notNull(),
+                    'dateCreated' => $this->dateTime()->notNull(),
+                    'dateUpdated' => $this->dateTime()->notNull(),
+                    'uid' => $this->uid(),
+                ]
+            );
+        }
+
+        if($tablesCreated) {
+            $this->addForeignKey(
+                $this->db->getForeignKeyName(NotFoundRecord::tableName(), 'siteId'),
+                NotFoundRecord::tableName(),
+                'siteId',
+                '{{%sites}}',
+                'id',
+                'CASCADE'
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m191114_182559_addNotFoundTable cannot be reverted.\n";
+        return false;
+    }
+}
---
+++ src/models/NotFoundModel.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * SEO Fields plugin for Craft CMS 3.x
+ *
+ * Fields for your SEO & OG data
+ *
+ * @link      https://studioespresso.co
+ * @copyright Copyright (c) 2019 Studio Espresso
+ */
+
+namespace studioespresso\seofields\models;
+
+use craft\base\Model;
+use craft\validators\DateTimeValidator;
+
+/**
+ * @author    Studio Espresso
+ * @package   SEO Fields
+ * @since     1.0.0
+ */
+class NotFoundModel extends Model
+{
+    // Public Properties
+    // =========================================================================
+    public $url;
+
+    public $counter = '0';
+
+    public $dateLastHit;
+
+    public $handled = false;
+
+    public $siteId;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Returns the validation rules for attributes.
+     *
+     * Validation rules are used by [[validate()]] to check if attribute values are valid.
+     * Child classes may override this method to declare different validation rules.
+     *
+     * More info: http://www.yiiframework.com/doc-2.0/guide-input-validation.html
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [
+                ['counter', 'url', 'dateLastHit', 'handled', 'siteId'], 'required'
+            ],
+            ['counter, siteId', 'numerical'],
+            ['handled', 'boolean'],
+            ['dateLastHit', DateTimeValidator::class],
+            ['url', 'unique', ]
+        ];
+    }
+}
