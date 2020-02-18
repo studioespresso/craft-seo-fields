34192d86ae4f39f579380e093924a283b523f5fb
Jan Henckens <jan@studioespresso.co>
2020-02-18 18:27:59 +0100

    NL translation for most fields

---
+++ src/translations/nl/seo-fields.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    "Default site title" => "Standaard site naam",
+    "Default meta description" => "Standaard meta omschrijving",
+    "Title seperator" => "Scheidingsteken tussen pagina titel en site naam",
+    "A character used to seperate your entry title from the site name" => "Een teken of symbool om de pagina titel te scheiden van de naam van de site",
+    "Default meta image" => "Standaard meta afbeelding",
+    "What does a robots.txt file do?" => "Wat doet een robots.txt bestand?",
+    "A robots.txt file tells search engine crawlers which pages or files the crawler can or can't request from your site. This is used mainly to avoid overloading your site with requests; it is not a mechanism for keeping a web page out of Google." => "Een robots.txt bestand verteld een zoekmachine welke pagina's en bestanden hij wel of niet mag bezoeken op je site. Het is geen mechanisme om te zorgen dat een pagina niet vindbaar is op Google of andere zoekmachines",
+    "Enable robots.txt" => "Robots.txt inschakelen",
+    "Let the plugin handle your robots.txt" => "Laat SEO Fields je robots.txt beheren",
+    "Robots.txt content" => "Robots.txt inhoud",
+    "Pattern / URL" => "Patroon / URL",
+    "Redirect to" => "Redirect naar",
+    "Site" => "Site",
+    "Hits" => "Hits",
+    "Matched by" => "Gematched op",
+    "Last hit at" => "Laatste keer actief op",
+    "Method" => "Methode",
+    "Remove" => "Verwijderen",
+    "Last linked from" => "Laatste gelinkt vanaf"
+];
