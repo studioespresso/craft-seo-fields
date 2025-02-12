c6808cbd2c8c0f635673db9acbddcb7fdeb8c1b1
Jan Henckens <jan@henckens.be>
2025-02-12 20:46:55 +0100

    Added composer ci command
    Signed-off-by: Jan Henckens <jan@henckens.be>

--- src/controllers/CpApiController.php
+++ src/controllers/CpApiController.php
@@ -102,7 +102,7 @@ class CpApiController extends Controller
                     'field' => 'hits',
                     'sortField' => 'counter',
                     'direction' => 'desc',
-                ]
+                ],
             ];
         }
 
