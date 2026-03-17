62d8a7dcff02812cd4d74f6ac018eb430fbe2559
Jan Henckens <jan@henckens.be>
2026-03-17 20:14:46 +0100

    Make schema work for multisite

--- src/controllers/SchemaController.php
+++ src/controllers/SchemaController.php
@@ -3,8 +3,10 @@
 namespace studioespresso\seofields\controllers;
 
 use Craft;
+use craft\helpers\Cp;
 use craft\models\Site;
 use craft\web\Controller;
+use craft\web\Response;
 use studioespresso\seofields\models\SeoDefaultsModel;
 use studioespresso\seofields\SeoFields;
 
@@ -16,30 +18,53 @@ class SchemaController extends Controller
 
     public function init(): void
     {
-        parent::init();
-        $this->requirePermission('seo-fields:schema');
-
         if (Craft::$app->getRequest()->getQueryParam('site')) {
             $this->site = Craft::$app->getSites()->getSiteByHandle(Craft::$app->getRequest()->getQueryParam('site'));
         }
         if (!$this->site) {
             $this->site = Craft::$app->getSites()->getPrimarySite();
         }
+        parent::init();
     }
 
     public function actionIndex()
     {
+        $this->requirePermission('seo-fields:schema');
+
         $data = SeoFields::$plugin->defaultsService->getDataBySiteId($this->site->id);
         $sections = Craft::$app->getEntries()->getAllSections();
+        $sites = Craft::$app->getSites()->getEditableSites();
 
-        return $this->renderTemplate('seo-fields/_schema', [
-            'data' => $data,
-            'sitemapPerSite' => SeoFields::$plugin->getSettings()->sitemapPerSite,
-            'sections' => $sections,
-            'options' => SeoFields::getInstance()->schemaService->getDefaultOptions(),
-            'siteEntityOptions' => SeoFields::getInstance()->schemaService->getSiteEntityOptions(),
-            'selectedSite' => $this->site,
-        ]);
+        $siteCrumb = ['label' => $this->site->name];
+
+        if (Craft::$app->getIsMultiSite()) {
+            $siteCrumb['menu'] = [
+                'label' => Craft::t('site', 'Select site'),
+                'items' => Cp::siteMenuItems($sites, $this->site),
+            ];
+        }
+
+        return $this->asCpScreen()
+            ->title(Craft::t('seo-fields', 'Schema.org'))
+            ->selectedSubnavItem('schema')
+            ->crumbs([
+                ['label' => Craft::t('seo-fields', 'SEO Fields'), 'url' => 'seo-fields'],
+                $siteCrumb,
+            ])
+            ->action('seo-fields/schema/save')
+            ->redirectUrl('seo-fields/schema?site=' . $this->site->handle)
+            ->addAltAction(Craft::t('seo-fields', 'Save'), [
+                'shortcut' => true,
+                'retainScroll' => true,
+                'redirect' => 'seo-fields/schema?site=' . $this->site->handle,
+            ])
+            ->contentTemplate('seo-fields/_schema/_content', [
+                'data' => $data,
+                'sections' => $sections,
+                'options' => SeoFields::getInstance()->schemaService->getDefaultOptions(),
+                'siteEntityOptions' => SeoFields::getInstance()->schemaService->getSiteEntityOptions(),
+                'site' => $this->site,
+            ]);
     }
 
     public function actionSave()
@@ -47,14 +72,15 @@ class SchemaController extends Controller
         $this->requirePostRequest();
 
         $data = [];
-        if (Craft::$app->getRequest()->getBodyParam('id')) {
-            $model = SeoFields::$plugin->defaultsService->getDataById(Craft::$app->getRequest()->getBodyParam('id'));
-        } else {
+        $siteId = Craft::$app->getRequest()->getBodyParam('siteId', Craft::$app->getSites()->getPrimarySite()->id);
+
+        $model = SeoFields::$plugin->defaultsService->getDataBySiteId($siteId);
+        if (!$model) {
             $model = new SeoDefaultsModel();
         }
 
         $data['schema'] = Craft::$app->getRequest()->getBodyParam('data');
-        $data['siteId'] = Craft::$app->getRequest()->getBodyParam('siteId', Craft::$app->getSites()->getPrimarySite()->id);
+        $data['siteId'] = $siteId;
         $data['siteEntity'] = Craft::$app->getRequest()->getBodyParam('siteEntity');
         $data['organizationName'] = Craft::$app->getRequest()->getBodyParam('organizationName');
         $data['organizationLogo'] = Craft::$app->getRequest()->getBodyParam('organizationLogo');
