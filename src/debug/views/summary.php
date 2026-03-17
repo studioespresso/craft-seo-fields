52b68a4c58e7f00f9e2b851d11ca6b1cce9f31ae
Jan Henckens <jan@henckens.be>
2026-03-17 21:12:52 +0100

    Make the panel green if we don’t have any errors

--- src/debug/views/summary.php
+++ src/debug/views/summary.php
@@ -6,6 +6,6 @@ $warningCount = count($warnings);
 ?>
 <div class="yii-debug-toolbar__block">
     <a href="<?= $panel->getUrl() ?>">
-        Schema <span class="yii-debug-toolbar__label<?= $warningCount > 0 ? ' yii-debug-toolbar__label_warning' : '' ?>"><?= $nodes ?><?= $warningCount > 0 ? " ⚠ {$warningCount}" : '' ?></span>
+        Schema <span class="yii-debug-toolbar__label <?= $warningCount > 0 ? 'yii-debug-toolbar__label_warning' : 'yii-debug-toolbar__label_success' ?>"><?= $nodes ?><?= $warningCount > 0 ? " ⚠ {$warningCount}" : '' ?></span>
     </a>
 </div>
